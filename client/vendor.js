14e0a42e0dff51562f24a89b3ccdc9b73eb93277
sirdat1993 <sirdat1993@gmail.com>
2017-05-11 18:48:39 +0700

    fix

---
+++ client/vendor.js
@@ -0,0 +1,10 @@
+require('react');
+require('react-dom');
+require('react-redux');
+require('react-router');
+require('redux');
+require('redux-thunk');
+require('react-ga');
+require('react-intl');
+require('react-hot-loader');
+require('isomorphic-fetch');
