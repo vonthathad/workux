b46ff56aac8dc68c092af6ad1e8b8d6a5ac39b6b
sirdat1993 <sirdat1993@gmail.com>
2017-05-11 18:48:46 +0700

    fxi

--- client/modules/User/UserActions.js
+++ client/modules/User/UserActions.js
@@ -13,15 +13,15 @@ export function addUser(user) {
 
 export function fetchUser(token) {
   return (dispatch) => {
-    // return callApi(`token?access_token=${token}`).then(res => {
-    //   if (res.user) {
-    //     dispatch(addUser(res.user));
-    //     localStorage.setItem('token', res.user.token);
-    //   }
-    //   if (res.error) {
-    localStorage.removeItem('token');
-    location.href = location.href.split('?')[0];
-    //   }
-    // });
+    return callApi(`token?access_token=${token}`).then(res => {
+      if (res.user) {
+        dispatch(addUser(res.user));
+        localStorage.setItem('token', res.user.token);
+      }
+      if (res.error) {
+        localStorage.removeItem('token');
+        location.href = location.href.split('?')[0];
+      }
+    });
   };
 }
